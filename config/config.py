f623bba6ad203ece335c3755cf1ea5558595a8aa
CatalinaSg <Kattha16@gmail.com>
2025-02-05 22:26:07 -0500

    first commit

---
+++ config/config.py
@@ -0,0 +1,17 @@
+BASE_URL = "https://globalseguros--qaonb.sandbox.my.salesforce.com/"
+TARGET_URL = "https://globalseguros--qaonb.sandbox.lightning.force.com/lightning/cmp/vlocity_ins__vlocityLWCOmniWrapper?c__target=c%3AgsvFormularyEnglish&c__layout=lightning&c__tabLabel=Perfilamiento"
+
+USERNAME = "jcsuarez@ext.globalseguros.co"
+PASSWORD = "+18Stavros"
+
+# Otros valores configurables
+ASESOR = "PROYECTA-T LTDA-BOGOTA"
+NOMBRE= "TEST"
+PAPELLIDO="TEST"
+SAPELLIDO= "TEST"
+EMAIL= "test@example.com"
+CIUDAD= "BOGOTA"
+DEPARTAMENTO= "DISTRITO CAPITAL"
+PAIS= "Colombia"
+TELEFONO= "(312) 211-2112"
+EVENTO= "FERIA DEL LIBRO 2024"
